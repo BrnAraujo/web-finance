ff6e4644f592f5e05172d5842ddcffa03bdd4442
Bruno Pereira <bruno1774@hotmail.com>
2021-01-28 15:48:28 -0300

    Udpdate css

---
+++ index.js
@@ -0,0 +1,10 @@
+const Modal = {
+    open(){
+        document.querySelector(`.modal-overlay`)
+        .classList.add(`active`)
+    },
+    close(){
+        document.querySelector(`.modal-overlay`)
+        .classList.remove(`active`)
+    }
+}
